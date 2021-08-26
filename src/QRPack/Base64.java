02a07bca663af3c53f928e0346db2af8525e6f6f
AKRAME <akrame_djellouli@yahoo.fr>
2021-08-26 01:25:45 +0100

    test 1

---
+++ src/QRPack/Base64.java
@@ -0,0 +1,26 @@
+package QRPack;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class Base64 {
+	public static String encoder(String imagePath) {
+		String base64Image = "";
+		File file = new File(imagePath);
+		try (FileInputStream imageInFile = new FileInputStream(file)) {
+			// Reading a Image file from file system
+			byte imageData[] = new byte[(int) file.length()];
+			imageInFile.read(imageData);
+			
+			base64Image = String.valueOf(imageData.toString());
+		} catch (FileNotFoundException e) {
+			System.out.println("Image not found" + e);
+		} catch (IOException ioe) {
+			System.out.println("Exception while reading the Image " + ioe);
+		}
+		return base64Image;
+	}
+}
